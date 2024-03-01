17f5c071178e1bad28bbf52b1923bf4ef0f0f73b
Shaikha <shaikha.s.ebrahim@gmail.com>
2024-03-01 11:18:28 +0300

    About

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import Welcoming from "../components/Welcoming"
+import About from "../components/About"
 import InterestedServices from "../components/InterestedServices"
 
 const Home = () => {
   return (
     <div>
       <Welcoming />
+      <About />
       <InterestedServices />
     </div>
   )
