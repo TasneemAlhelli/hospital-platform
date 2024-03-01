2c67664fe2e6a1c2985ef697fe37e1fba1391f38
zahraa <zahraaalihasan279@gmail.com>
2024-03-01 11:47:30 +0300

    profile data

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
-import Home from './pages/Home'
-import Login from './pages/Login'
-import Register from './pages/Register'
-import Appointment from './pages/Appointment'
-import Service from './pages/Service'
-import Services from './pages/Services'
-import Doctors from './pages/Doctors'
-import DoctorDetails from './pages/DoctorDetails'
-import EditProfile from './pages/EditProfile'
+// import Home from './pages/Home'
+// import Login from './pages/Login'
+// import Register from './pages/Register'
+// import Appointment from './pages/Appointment'
+// import Service from './pages/Service'
+// import Services from './pages/Services'
+// import Doctors from './pages/Doctors'
+// import DoctorDetails from './pages/DoctorDetails'
+// import EditProfile from './pages/EditProfile'
 import Profile from './pages/Profile'
 function App() {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Home />} />
+        {/* <Route path="/" element={<Home />} />
         <Route path="/signin" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/book-an-appointment" element={<Appointment />} />
@@ -22,8 +22,8 @@ function App() {
         <Route path="/services" element={<Services />} />
         <Route path="/doctors" element={<Doctors />} />
         <Route path="/doctors/:doctorId" element={<DoctorDetails />} />
-        <Route path="/profile/:userrId/edit" element={<EditProfile />} />
-        <Route path="/profile/:userrId" element={<Profile />} />
+        <Route path="/profile/:userrId/edit" element={<EditProfile />} /> */}
+        <Route path="/profile/:userId" element={<Profile />} />
       </Routes>
     </div>
   )
