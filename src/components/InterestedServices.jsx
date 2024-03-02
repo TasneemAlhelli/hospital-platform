c801281be9cda98f794ba5b86f694850fa1bc668
zahraa <zahraaalihasan279@gmail.com>
2024-03-02 12:58:02 +0300

    set Interested Services from backend

--- src/components/InterestedServices.jsx
+++ src/components/InterestedServices.jsx
@@ -1,9 +1,22 @@
+import { getInterestedServices } from '../services/services'
+
 const InterestedServices = () => {
+  const [interestedServices, setInterestedServices] = useState([])
+
+  useEffect(() => {
+    getAllServices()
+  }, [])
+
+  const getAllServices = async () => {
+    const services = await getInterestedServices()
+    setInterestedServices(services)
+  }
+
   return (
     <div>
       <section>
         <h1>Services</h1>
-        </section>
+      </section>
     </div>
   )
 }
--- src/services/services.js
+++ src/services/services.js
@@ -9,6 +9,15 @@ export const getServices = async () => {
   }
 }
 
+export const getInterestedServices = async () => {
+  try {
+    const res = await Client.get('/services/filter')
+    return res.data
+  } catch (error) {
+    console.log(error)
+  }
+}
+
 export const getService = async (serviceId) => {
   try {
     const res = await Client.get(`/services/${serviceId}`)
