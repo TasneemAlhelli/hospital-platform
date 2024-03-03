772fbf428a9b8cac9c22acce8419211dbb6eb5a5
zahraa <zahraaalihasan279@gmail.com>
2024-03-03 10:18:35 +0300

    edit in login page state

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,24 @@
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import { useState } from 'react'
 
-const Login = () => {
+const Login = ({ setUser }) => {
+  let navigate = useNavigate()
+  const [formValues, setFormValues] = useState({
+    email: '',
+    password: ''
+  })
+
+  const handleChange = (e) => {
+    setFormValues({ ...formValues, [e.target.name]: e.target.value })
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    const payload = await SignInUser(formValues)
+    setFormValues({ email: '', password: '' })
+    setUser(payload)
+    navigate('/')
+  }
   return (
     <div>
       <h1>Login </h1>
