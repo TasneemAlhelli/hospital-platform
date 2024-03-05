15c21b2e280f330eeead517a3a5b2a8f93a1773d
Shaikha <shaikha.s.ebrahim@gmail.com>
2024-03-05 11:37:44 +0300

    Style Profile Page & Appointment Section

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,10 +1,10 @@
-import { useParams, Link } from 'react-router-dom'
+import { useParams, Link } from "react-router-dom"
 import {
   getAppointments,
   getUserInfo,
-  getAppointmentsStatus
-} from '../services/appointments'
-import { useState, useEffect } from 'react'
+  getAppointmentsStatus,
+} from "../services/appointments"
+import { useState, useEffect } from "react"
 
 const Profile = () => {
   // let { userId } = useParams()
@@ -32,7 +32,7 @@ const Profile = () => {
   const userInfo = async () => {
     const data = await getUserInfo()
     setProfile(data)
-    console.log('user data', data)
+    console.log("user data", data)
     // let currentDate = new Date()
     // let birthDate = new Date(data.birthDate)
     // let timeDiff = currentDate.getTime() - birthDate.getTime()
@@ -47,13 +47,57 @@ const Profile = () => {
   }, [])
 
   return (
-    <div>
-      <h1>Profile</h1>
-      <Link to="/profile/edit">
-        <button>Edit prfile :</button>
-      </Link>
+    <div className="profilePosition">
+      <h1 className="profileTitle">Profile</h1>
+      <div className="profileCard">
+        <div>
+          <Link to="/profile/edit">
+            <button className="profileBtn">Edit Profile</button>
+          </Link>
+        </div>
+        <div>
+          <h3>Patient Name</h3>
+          <img src="" alt="" />
+          <p>Phone Number: </p>
+          <p>Email: </p>
+          <p>Gender: </p>
+          <p>Experience: 12 years</p>
+        </div>
+      </div>
 
-      
+      <h1 className="appTitle">Appointments</h1>
+      <div className="profileCard">
+        <div className="btn-container">
+          <label className="switch btn-color-mode-switch">
+            <input
+              value="1"
+              id="color_mode"
+              name="color_mode"
+              type="checkbox"
+            />
+            <label
+              className="btn-color-mode-switch-inner"
+              data-off="Completed"
+              data-on="Scheduled"
+              for="color_mode"
+            ></label>
+          </label>
+        </div>
+        <div className="banner">
+          <div class="reviewsCard">
+            <div class="imgWrapper">
+              <img src="./public/image/icons8-schedule-64.png"></img>
+            </div>
+
+            <div class="reviewWrapper">
+              <span class="reviewText">Description</span>
+              <p class="review">pspspspsps</p>
+            </div>
+
+            <button class="reviewBtn">Review</button>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
