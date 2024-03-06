7264494e53b27e28740d19d83a5bab0f3218d44d
Shaikha <shaikha.s.ebrahim@gmail.com>
2024-03-06 17:15:35 +0300

    footer style

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,9 +4,27 @@ const Footer = () => {
       <section className="footerSec">
         <div className="footerContainer">
           <div className="social-buttons">
-            {/* <a href="#" className="social-button github"></a>
-            <a href="#" className="social-button github"></a>
-            <a href="#" className="social-button github"></a> */}
+            <div>
+              <p>Tel: +973 66966371</p>
+              <p>&copy; 2024 Your Website Name. All rights reserved.</p>
+            </div>
+            <div className="footerNav">
+              <a href="#">About</a>
+              <a href="#">Contact</a>
+              <a href="#">Terms of Service</a>
+              <a href="#">Privacy Policy</a>
+            </div>
+            <div>
+              <a href="#" target="_blank" rel="noopener noreferrer">
+                Facebook
+              </a>
+              <a href="#" target="_blank" rel="noopener noreferrer">
+                Twitter
+              </a>
+              <a href="#" target="_blank" rel="noopener noreferrer">
+                Instagram
+              </a>
+            </div>
           </div>
         </div>
       </section>
