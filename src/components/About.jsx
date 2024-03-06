1990005806bf69c03d67e6032ef109b87ed38747
Shaikha <shaikha.s.ebrahim@gmail.com>
2024-03-06 18:03:48 +0300

    footer style

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,10 +3,11 @@ const About = () => {
     <div>
       <section className="aboutSec">
         <div className="aboutContainer">
-          <h2>About us</h2>
+          <h2>About Us</h2>
           <p>
           We're a leading medical institution driven by a shared passion for healing. <br />Our dedicated team of professionals, armed with cutting-edge technology, delivers top-notch <br />care across specialties. Beyond clinical excellence, we prioritize compassion and lasting <br />relationships. At General Assembly Hospital, your well-being is our unwavering commitment.
           </p>
+          
         </div>
       </section>
     </div>
